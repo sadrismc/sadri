70e0d18a64f5479ab5307a9610b5e064b0057a53
sadrismc <sadrismc@gmail.com>
2026-04-17 04:29:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ const path = require("path");
 
 const app = express();
 
-// 👇 THIS makes your public_html folder work
-app.use(express.static(path.join(__dirname, "public_html")));
+// Serve static files from current folder
+app.use(express.static(__dirname));
 
-// 👇 Force homepage
+// Home route → index.html
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public_html", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// Optional: handle 404 properly
+app.use((req, res) => {
+  res.status(404).send("404 - Page Not Found");
+});
+
+// IMPORTANT: Render uses dynamic port
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
